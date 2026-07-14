cf6e0f5a00bf3eef5feaac145ddc5850f11619e3
Codingspace-creator <drpritis02@gmail.com>
2026-07-14 17:47:36 +0530

    Activity 4

--- Lesson 2/Classroom activities/main.py
+++ Lesson 2/Classroom activities/main.py
@@ -2,4 +2,3 @@ print ("Hello world")
 print (5)
 name=input("Who is your favourite favourite football player ")
 print ("my favourite football player", name)
-
---
+++ Lesson 4/Main.py
@@ -0,0 +1,11 @@
+field1 = 120
+field2 = 85
+field3 = 150
+field4 = 95
+field5 = 110
+total = field1 + field2 + field3 + field4 + field5
+average = field1 + field2 + field3 + field4 + field5 / 5
+bags = total // 25
+Grainsleft = total % 25
+print ("Total bags of 25 KG" , bags)
+print ("Total grains left" , Grainsleft)
